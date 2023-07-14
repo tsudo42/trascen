a402f72b960121c0dd4ad84be6f7c5a5110129aa
Anonymous <anonymous@example.com>
2023-07-15 02:08:54 +0900

    :recycle: 使用していない関数 GetPosts() を削除する

--- srcs/frontend/app/page.tsx
+++ srcs/frontend/app/page.tsx
@@ -2,22 +2,8 @@ import { getAllPosts } from '../utils/api'
 import PostComponent from './post'
 import { PostType } from '../utils/Types'
 
-
-export async function GetPosts() {
-  const posts: PostType[] = await getAllPosts();
-
-  return {
-    props: {
-      posts,
-    },
-  };
-}
-
 const Page = async () => {
-
   const posts: PostType[] = await getAllPosts();
-  // set props from getStaticProps
-  // require props  
   return <PostComponent posts={posts} />
 }
 
