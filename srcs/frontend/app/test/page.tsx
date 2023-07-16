151d1c086604397be094b013374cf5cd9fdcdfad
tsudo42 <91622558+tsudo42@users.noreply.github.com>
2023-07-17 02:07:45 +0900

    :sparkles: app routingでの42認証

---
+++ srcs/frontend/app/test/page.tsx
@@ -0,0 +1,7 @@
+export default async function Home() {
+	return (
+		<main>
+			<p>This is a test page.</p>
+		</main>
+	);
+}
---
+++ srcs/frontend/process.d.ts
@@ -0,0 +1,6 @@
+declare namespace NodeJS { // eslint-disable-line no-unused-vars
+	export interface ProcessEnv {
+		FORTY_TWO_CLIENT_ID: string
+		FORTY_TWO_CLIENT_SECRET: string
+	}
+}
