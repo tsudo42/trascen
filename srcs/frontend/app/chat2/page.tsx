293e70c7c8616f9d4966a05f88cac3938b8f27b4
Anonymous <anonymous@example.com>
2023-07-16 21:46:32 +0900

    :bug: sidebar が navbar と重なってしまっていたため修正

--- srcs/frontend/app/chat2/page.tsx
+++ srcs/frontend/app/chat2/page.tsx
@@ -6,7 +6,7 @@ const Messages = () => {
     <>
       <aside
         id="separator-sidebar"
-        className="fixed left-0 top-0 z-40 h-screen w-64 -translate-x-full transition-transform sm:translate-x-0"
+        className="left-0 top-0 z-40 h-screen w-64 -translate-x-full transition-transform sm:translate-x-0"
         aria-label="Sidebar"
       >
         <div className="h-full overflow-y-auto bg-gray-800 px-3 py-4 ">
