572665ff2ab9fecf748f1dafbce01a7c3269bd32
Anonymous <anonymous@example.com>
2023-07-18 21:48:50 +0900

    :+1:  header 部分の UI 実装

--- srcs/frontend/app/chat2/channel_category.tsx
+++ srcs/frontend/app/chat2/channel_category.tsx
@@ -1,6 +1,6 @@
 const ChannelCategory = ({ categoryName }: { categoryName: string }) => {
   return (
-    <span className="flex p-2 text-xs font-bold uppercase text-gray-500">
+    <span className="flex p-2 font-bold uppercase text-gray-500">
       {categoryName}
     </span>
   );
