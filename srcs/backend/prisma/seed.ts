86ab221cff651608cd57f2b27a6d2a9a34ac7319
tsudo42 <91622558+tsudo42@users.noreply.github.com>
2023-07-23 04:10:02 +0900

    :+1: データベースにemailを追加

--- srcs/backend/prisma/seed.ts
+++ srcs/backend/prisma/seed.ts
@@ -1,26 +1,24 @@
 import { PrismaClient } from '@prisma/client';
-import { hash } from 'bcrypt';
+// import { hash } from 'bcrypt';
 
 const prisma = new PrismaClient();
 
 async function main() {
-  const hashedPassword1 = await hash('password1', 10);
-  const user1 = await prisma.user.create({
-    data: {
-      username: 'Alice',
-      password: hashedPassword1,
-    },
-  });
-
-  const hashedPassword2 = await hash('password2', 10);
-  const user2 = await prisma.user.create({
-    data: {
-      username: 'Bob',
-      password: hashedPassword2,
-    },
-  });
-
-  console.log({ user1, user2 });
+  // const hashedPassword1 = await hash('password1', 10);
+  // const user1 = await prisma.user.create({
+  //   data: {
+  //     username: 'Alice',
+  //     password: hashedPassword1,
+  //   },
+  // });
+  // const hashedPassword2 = await hash('password2', 10);
+  // const user2 = await prisma.user.create({
+  //   data: {
+  //     username: 'Bob',
+  //     password: hashedPassword2,
+  //   },
+  // });
+  // console.log({ user1, user2 });
 }
 
 main()
