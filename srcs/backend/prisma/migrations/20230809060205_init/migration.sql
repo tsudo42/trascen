17d9afb0fc0a590e6d45c8f1e99ede61f4dd44b8
Anonymous <anonymous@example.com>
2023-08-09 15:47:00 +0900

    :sparkles:スキーマのユーザ情報などを外部制約化

---
+++ srcs/backend/prisma/migrations/20230809060205_init/migration.sql
@@ -0,0 +1,75 @@
+-- CreateEnum
+CREATE TYPE "Publicity" AS ENUM ('PUBLIC', 'PRIVATE');
+
+-- CreateEnum
+CREATE TYPE "UserType" AS ENUM ('OWNER', 'ADMIN', 'USER');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ChatChannels" (
+    "channelId" SERIAL NOT NULL,
+    "channelName" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL,
+    "channelType" "Publicity" NOT NULL,
+    "hashedPassword" TEXT,
+
+    CONSTRAINT "ChatChannels_pkey" PRIMARY KEY ("channelId")
+);
+
+-- CreateTable
+CREATE TABLE "ChatChannelUsers" (
+    "channelUserId" SERIAL NOT NULL,
+    "channelId" INTEGER NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "type" "UserType" NOT NULL,
+
+    CONSTRAINT "ChatChannelUsers_pkey" PRIMARY KEY ("channelUserId")
+);
+
+-- CreateTable
+CREATE TABLE "ChatMessages" (
+    "messageId" SERIAL NOT NULL,
+    "channelId" INTEGER NOT NULL,
+    "senderId" INTEGER NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "ChatMessages_pkey" PRIMARY KEY ("messageId")
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "author" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ChatChannels_channelName_key" ON "ChatChannels"("channelName");
+
+-- AddForeignKey
+ALTER TABLE "ChatChannelUsers" ADD CONSTRAINT "ChatChannelUsers_channelId_fkey" FOREIGN KEY ("channelId") REFERENCES "ChatChannels"("channelId") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ChatChannelUsers" ADD CONSTRAINT "ChatChannelUsers_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ChatMessages" ADD CONSTRAINT "ChatMessages_channelId_fkey" FOREIGN KEY ("channelId") REFERENCES "ChatChannels"("channelId") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ChatMessages" ADD CONSTRAINT "ChatMessages_senderId_fkey" FOREIGN KEY ("senderId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ srcs/backend/src/chats/chats.interface.ts
@@ -0,0 +1,11 @@
+
+export enum Publicity {
+  PUBLIC  = 'PUBLIC',
+  PRIVATE = 'PRIVATE',
+}
+
+export enum UserType {
+  OWNER  = 'OWNER',
+  ADMIN  = 'ADMIN',
+  USER   = 'USER',
+}
