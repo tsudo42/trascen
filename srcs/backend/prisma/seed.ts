47edaeae5b147b9171083d481eeaf142e5420ed2
tsudo42 <91622558+tsudo42@users.noreply.github.com>
2023-08-12 04:43:33 +0900

    :+1: Profile, Authに必要なスキーマ追加

--- srcs/backend/prisma/seed.ts
+++ srcs/backend/prisma/seed.ts
@@ -1,24 +1,34 @@
 import { PrismaClient } from '@prisma/client';
-// import { hash } from 'bcrypt';
+import { hash } from 'bcrypt';
 
 const prisma = new PrismaClient();
 
 async function main() {
-  // const hashedPassword1 = await hash('password1', 10);
-  // const user1 = await prisma.user.create({
-  //   data: {
-  //     username: 'Alice',
-  //     password: hashedPassword1,
-  //   },
-  // });
-  // const hashedPassword2 = await hash('password2', 10);
-  // const user2 = await prisma.user.create({
-  //   data: {
-  //     username: 'Bob',
-  //     password: hashedPassword2,
-  //   },
-  // });
-  // console.log({ user1, user2 });
+  const hashedPassword1 = await hash('password1', 10);
+  const user1 = await prisma.user.create({
+    data: {
+      email: 'alice@example.com',
+      username: 'Alice',
+      staff: true,
+      password: hashedPassword1,
+      profile: {
+        create: { bio: 'Hi! This is Alice.' },
+      },
+    },
+  });
+  const hashedPassword2 = await hash('password2', 10);
+  const user2 = await prisma.user.create({
+    data: {
+      email: 'bob@example.com',
+      username: 'Bob',
+      staff: true,
+      password: hashedPassword2,
+      profile: {
+        create: { bio: 'Hi! This is Bob.' },
+      },
+    },
+  });
+  console.log({ user1, user2 });
 }
 
 main()
