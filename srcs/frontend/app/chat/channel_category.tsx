79b45523b5a39b04ee1f2168e929650d38a18d8c
Anonymous <anonymous@example.com>
2023-09-08 00:35:45 +0900

    :recycle: channel create modal のリファクタ

--- srcs/frontend/app/chat/channel_category.tsx
+++ srcs/frontend/app/chat/channel_category.tsx
@@ -1,17 +1,14 @@
-import { useCallback } from "react";
+import { use, useCallback, useEffect } from "react";
 import useModal from "../components/useModal";
 
 
 const ChannelCategory = ({ categoryName }: { categoryName: string }) => {
   const { ref, showModal, closeModal } = useModal();
 
-  // dialog の外側をクリックしたときに閉じるために使用する
-  const stopPropagation = useCallback(
-    (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-      e.stopPropagation();
-    },
-    [],
-  );
+  // チャンネル作成ボタンをクリックしたときのハンドラ
+  useEffect(() => {
+    console.log("ChannelCategory useEffect");
+  }, []);
 
   return (
     <span className="flex flex-row items-center justify-between p-2 font-bold uppercase text-gray-500">
@@ -35,27 +32,45 @@ const ChannelCategory = ({ categoryName }: { categoryName: string }) => {
         style={{ top: "30px" }}
         className="rounded-lg bg-gray-600"
       >
-        {/* NOTE: dialog の中身 */}
-        <div onClick={stopPropagation} className="flex flex-col text-white px-6 py-2">
-          <div className="text-lg">Create Channel</div>
-          <div className="flex flex-row justify-between">
-            <div className="text-sm">Channel Name: </div>
-            <input
-              className="rounded-md bg-gray-500 px-2 text-white"
-              type="text"
-              placeholder="Channel Name"
-            />
-          </div>
-          <div className="flex flex-row justify-end">
-            <button className="m-2 rounded-md bg-gray-400 px-2 text-white">
-              Create
-            </button>
-            <button className="m-2 rounded-md bg-gray-500 px-2 text-white">
-              Cancel
-            </button>
-          </div>
-        </div>
+        <ChannelCreateModal closeModal={closeModal} />
       </dialog>
     </span>
   );
 };
+
+const ChannelCreateModal = ({ closeModal }: any) => {
+  // dialog の外側をクリックしたときに閉じるために使用する
+  const stopPropagation = useCallback(
+    (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+      e.stopPropagation();
+    },
+    [],
+  );
+
+
+  return (
+    <div onClick={stopPropagation} className="flex flex-col text-white px-6 py-2">
+      <div className="text-lg">Create Channel</div>
+      <div className="flex flex-row justify-between">
+        <div className="text-sm">Channel Name: </div>
+        <input
+          className="rounded-md bg-gray-500 px-2 text-white"
+          type="text"
+          placeholder="Channel Name"
+        />
+      </div>
+      <div className="flex flex-row justify-end">
+        <button
+          className="m-2 rounded-md bg-gray-400 px-2 text-white">
+          Create
+        </button>
+        <button onClick={closeModal}
+          className="m-2 rounded-md bg-gray-500 px-2 text-white">
+          Cancel
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default ChannelCategory;
