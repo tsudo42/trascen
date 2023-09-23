50e132a871e77209e8c937331f6c05011d18bde9
Anonymous <anonymous@example.com>
2023-09-23 17:25:03 +0900

    commit

--- srcs/frontend/app/profile/me/friends/friendsList.tsx
+++ srcs/frontend/app/profile/me/friends/friendsList.tsx
@@ -24,26 +24,28 @@ const FriendComponent = ({ followee }: {
 
   return (
     <>
-      <li>
+      <div>
         <a
           href="#"
-          className="group flex items-center rounded-lg p-2 text-white hover:bg-gray-700"
+          className="group flex items-center rounded-lg p-2 text-white"
         >
           <img
             src="http://localhost:3000/favicon.ico"
             className="h-auto max-w-full rounded-full"
-            width={30}
-            height={30}
+            width={45}
+            height={45}
             alt=""
             onClick={openMUserOps}
             cursor-pointer
           /> 
-          <span className="ml-3 shrink-0 pr-8 text-xl">
+          <div className="ml-3 shrink-0 pr-8 text-xl">
             {followee.username}
-          
-          </span>
+              <div className="tracking-[0.1em] text-darkgray-200">
+            offline
+          </div>
+          </div>
         </a>
-      </li>
+      </div>
       {isMUserOpsOpen && (
         <ModalPopup
           overlayColor="rgba(113, 113, 113, 0.3)"
@@ -63,7 +65,7 @@ const FriendsList = () => {
 
   useEffect(() => {
     if (profile.userId != "") {
-      // friends一覧を取得
+      // followees一覧を取得
       makeAPIRequest<FolloweeType[]>("get", `/friends/follow/followees/${profile.userId}`)
         .then((result) => {
           if (result.success) {
@@ -76,19 +78,14 @@ const FriendsList = () => {
           console.error("Error:", error.message);
         });
     }
-  }, [profile]); // has to change it to update the list
+  }, [profile]); 
 
   return (
     <>
       <div
-        className="absolute left-[470px] top-[400px] text-left font-body text-xl overflow-y-auto text-white"
+        className="absolute left-[470px] top-[400px] text-left text-xl "
       >
-        {/* className="left-0 top-0 z-40 h-screen w-64 translate-x-0 transition-transform" */}
-        
-        {/* <div className="relative h-full bg-darkslategray-100 px-3 py-4">   */}
-          {/* check color/ */}
-
-      <ul className="divide-y divide-gray-500/30">
+      <ul className="border-b-8">
         {followees?.map((followee) => (
           <FriendComponent
             key={followee.id}
@@ -96,26 +93,6 @@ const FriendsList = () => {
           />
         ))}
         </ul> 
-      
-
-        {/* <div className="absolute left-[0px] top-[84px] h-[75px] w-[500px]">
-          <div className="absolute left-[80px] top-[0px] tracking-[0.1em]">
-            user2
-          </div>
-          <div className="absolute left-[80px] top-[23px] tracking-[0.1em] text-darkgray-200">
-            Online
-          </div>
-          <img
-            className="absolute left-[0px] top-[73px] h-0.5 w-[500px]"
-            alt=""
-            src="/line-2.svg"
-          />
-          <img
-            className="absolute left-[21px] top-[0px] h-[45px] w-[45px]"
-            alt=""
-            src={icon2}
-            onClick={openMUserOps}
-          /> */}
         </div>
     </>
   );
