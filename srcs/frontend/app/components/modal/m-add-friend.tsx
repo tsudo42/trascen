440d6f46f66c6df2c0976c532da203cc63a3eff8
Anonymous <anonymous@example.com>
2023-09-24 18:03:47 +0900

    :+1: add friendのモダールのテキスト入力から情報を取得、ユーザー情報の抜き取り

--- srcs/frontend/app/components/modal/m-add-friend.tsx
+++ srcs/frontend/app/components/modal/m-add-friend.tsx
@@ -1,12 +1,79 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 import type { NextPage } from "next";
 import { TextField } from "@mui/material";
+import makeAPIRequest from '@/app/api/api';
 
 type MAddFriendType = {
   onClose?: () => void;
 };
 
+export type UserType = {
+  id: number;
+  username: string;
+  avatar: Uint8Array;
+  email: string;
+  staff: boolean;
+  password: string;
+  twoFactorAuthEnabled: boolean;
+  twoFactorAuthSecret: string;
+};
+
 const MAddFriend: NextPage<MAddFriendType> = ({ onClose }) => {
+
+  const [inputValue, setInputValue] = useState('');
+  const [users, setUsers] = useState<UserType[]>();
+  // const [user, setUser] = useState<UserType>();
+  let msg: string = "";
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleAddClick = () => {
+    const input :string = inputValue;
+    
+    useEffect(() => {
+      //user一覧の取得
+      makeAPIRequest<UserType[]>("get", `/users`)
+        .then((result) => {
+          if (result.success) {
+            setUsers(result.data);
+          } else {
+            console.error(result.error);
+          }
+        })
+        .catch((error) => {
+          console.error("Error:", error.message);
+        });
+    }, []);
+    
+    const found :UserType | undefined = users?.find((element) => element.username === input);
+    if (found) {
+      useEffect(() => {
+        //フレンズに追加
+        //    makeAPIRequest("post", "/auth/2fa", { userId: userId, code: otp })
+      // .then((result) => {
+
+        makeAPIRequest<UserType>("post", `/friends/follow`, { followeeId: found.id})
+          .then((result) => {
+            if (result.success) {
+              msg = "Added!";
+            } else {
+              console.error(result.error);
+            }
+          })
+          .catch((error) => {
+            console.error("Error:", error.message);
+          });
+    
+      }, []);
+    }
+    else {
+      msg = "Name not found"
+    }
+    setInputValue('');
+  };
+  
   return (
     <div className="relative h-[280px] max-h-full w-[390px] max-w-full overflow-hidden bg-darkslategray-100 text-left font-body text-5xl text-white">
       <div className="absolute left-[32px] top-[50px] tracking-[0.1em]">
@@ -22,9 +89,10 @@ const MAddFriend: NextPage<MAddFriendType> = ({ onClose }) => {
         label="friend name"
         size="medium"
         margin="none"
+        onChange={handleInputChange}
         required
       />
-      <button className="absolute left-[75px] top-[181px] h-[42px] w-[253px] cursor-pointer bg-[transparent] p-0 [border:none]">
+      <button className="absolute left-[75px] top-[181px] h-[42px] w-[253px] cursor-pointer bg-[transparent] p-0 [border:none]" onClick={handleAddClick}>
         <img
           className="absolute left-[0px] top-[0px] h-[41px] w-[240.84px]"
           alt=""
@@ -34,6 +102,9 @@ const MAddFriend: NextPage<MAddFriendType> = ({ onClose }) => {
           Add friend
         </div>
       </button>
+      <div>
+        {msg}
+      </div>
     </div>
   );
 };
