e4aa9d59b9fcbd61a1c16e09677de3ca91b733b9
Anonymous <anonymous@example.com>
2023-09-24 19:22:17 +0900

    :+1: フレンズ登録できた場合と、名前が見つからない場合の表示を追加

--- srcs/frontend/app/components/modal/m-add-friend.tsx
+++ srcs/frontend/app/components/modal/m-add-friend.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import type { NextPage } from "next";
 import { TextField } from "@mui/material";
-import makeAPIRequest from '@/app/api/api';
+import makeAPIRequest from "@/app/api/api";
 
 type MAddFriendType = {
   onClose?: () => void;
@@ -19,25 +19,45 @@ export type UserType = {
 };
 
 const MAddFriend: NextPage<MAddFriendType> = ({ onClose }) => {
-
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
+  const [msg, setMsg] = useState("");
   const [users, setUsers] = useState<UserType[]>();
-  // const [user, setUser] = useState<UserType>();
-  let msg: string = "";
+
+  useEffect(() => {
+    //user一覧の取得
+    makeAPIRequest<UserType[]>("get", `/users`)
+      .then((result) => {
+        if (result.success) {
+          console.log("get_users, success");
+          setUsers(result.data);
+        } else {
+          console.error(result.error);
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error.message);
+      });
+  }, []);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(event.target.value);
   };
 
   const handleAddClick = () => {
-    const input :string = inputValue;
-    
-    useEffect(() => {
-      //user一覧の取得
-      makeAPIRequest<UserType[]>("get", `/users`)
+    const input: string = inputValue;
+    const found: UserType | undefined = users?.find((element) => {
+      return element.username === input;
+    });
+    console.log("found", found);
+    if (found) {
+      //フレンズに追加
+      makeAPIRequest<UserType>("post", `/friends/follow`, {
+        followeeId: found.id,
+      })
         .then((result) => {
           if (result.success) {
-            setUsers(result.data);
+            setMsg("Added!");
+            console.log("added!");
           } else {
             console.error(result.error);
           }
@@ -45,35 +65,13 @@ const MAddFriend: NextPage<MAddFriendType> = ({ onClose }) => {
         .catch((error) => {
           console.error("Error:", error.message);
         });
-    }, []);
-    
-    const found :UserType | undefined = users?.find((element) => element.username === input);
-    if (found) {
-      useEffect(() => {
-        //フレンズに追加
-        //    makeAPIRequest("post", "/auth/2fa", { userId: userId, code: otp })
-      // .then((result) => {
-
-        makeAPIRequest<UserType>("post", `/friends/follow`, { followeeId: found.id})
-          .then((result) => {
-            if (result.success) {
-              msg = "Added!";
-            } else {
-              console.error(result.error);
-            }
-          })
-          .catch((error) => {
-            console.error("Error:", error.message);
-          });
-    
-      }, []);
+    } else {
+      setMsg("Name not found");
     }
-    else {
-      msg = "Name not found"
-    }
-    setInputValue('');
+    console.log("msg;", msg);
+    setInputValue("");
   };
-  
+
   return (
     <div className="relative h-[280px] max-h-full w-[390px] max-w-full overflow-hidden bg-darkslategray-100 text-left font-body text-5xl text-white">
       <div className="absolute left-[32px] top-[50px] tracking-[0.1em]">
@@ -92,7 +90,10 @@ const MAddFriend: NextPage<MAddFriendType> = ({ onClose }) => {
         onChange={handleInputChange}
         required
       />
-      <button className="absolute left-[75px] top-[181px] h-[42px] w-[253px] cursor-pointer bg-[transparent] p-0 [border:none]" onClick={handleAddClick}>
+      <button
+        className="absolute left-[75px] top-[181px] h-[42px] w-[253px] cursor-pointer bg-[transparent] p-0 [border:none]"
+        onClick={handleAddClick}
+      >
         <img
           className="absolute left-[0px] top-[0px] h-[41px] w-[240.84px]"
           alt=""
@@ -102,7 +103,7 @@ const MAddFriend: NextPage<MAddFriendType> = ({ onClose }) => {
           Add friend
         </div>
       </button>
-      <div>
+      <div className="absolute left-[100px] top-[230px] text-darkgray-200">
         {msg}
       </div>
     </div>
