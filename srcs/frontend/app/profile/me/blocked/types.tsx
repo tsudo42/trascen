e9843d99d08b767592eb4bc97744318a06c4e19f
Anonymous <anonymous@example.com>
2023-09-24 15:37:28 +0900

    :+1: Blockedリストの作成

---
+++ srcs/frontend/app/profile/me/blocked/types.tsx
@@ -0,0 +1,22 @@
+export type StatusType = {
+  userId: number;
+  socketId: string;
+  status: string;
+};
+
+export type ProfileType = {
+  id: number;
+  bio: string;
+  userId: number;
+};
+
+export type UserType = {
+  id: number;
+  username: string;
+  avatar: Uint8Array;
+  email: string;
+  staff: boolean;
+  password: string;
+  twoFactorAuthEnabled: boolean;
+  twoFactorAuthSecret: string;
+};
