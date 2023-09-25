f713f5d039c50ba58a766f717dc52a5996aafcba
tsudo42 <91622558+tsudo42@users.noreply.github.com>
2023-09-25 23:19:51 +0900

    :shower: ユーザの編集・削除の処理を消去

--- srcs/backend/src/users/users.service.ts
+++ srcs/backend/src/users/users.service.ts
@@ -1,17 +1,9 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly prisma: PrismaService) {}
-
-  async create(createUserDto: CreateUserDto) {
-    return await this.prisma.user.create({
-      data: createUserDto,
-    });
-  }
 
   async findAll() {
     return await this.prisma.user.findMany();
@@ -19,23 +11,6 @@ export class UsersService {
 
   async findOne(id: number) {
     const user = await this.prisma.user.findUnique({
-      where: { id: id },
-    });
-    if (!user) throw new NotFoundException();
-    return user;
-  }
-
-  async update(id: number, updateUserDto: UpdateUserDto) {
-    const user = await this.prisma.user.update({
-      where: { id: id },
-      data: updateUserDto,
-    });
-    if (!user) throw new NotFoundException();
-    return user;
-  }
-
-  async remove(id: number) {
-    const user = await this.prisma.user.delete({
       where: { id: id },
     });
     if (!user) throw new NotFoundException();
