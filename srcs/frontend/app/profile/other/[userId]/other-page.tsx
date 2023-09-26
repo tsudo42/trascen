e58c7b0cead41d028ec11c727cef928d8089b482
Anonymous <anonymous@example.com>
2023-09-26 17:01:17 +0900

    :recycle: format

--- srcs/frontend/app/profile/other/[userId]/other-page.tsx
+++ srcs/frontend/app/profile/other/[userId]/other-page.tsx
@@ -10,7 +10,7 @@ import RankingContainer from "@/app/components/raking-container";
 import MatchHistoryContainer from "@/app/components/match-history-container";
 import { StatusType } from "../../me/blocked/types";
 
-const ProfileOtherPage = ({ userId }: any ) => {
+const ProfileOtherPage = ({ userId }: any) => {
   const [user, setUser] = useState<UserType>();
   const [icon, setIcon] = useState<string>(
     "http://localhost:3000/api/users/avatar/0",
@@ -18,7 +18,7 @@ const ProfileOtherPage = ({ userId }: any ) => {
   const [status, setStatus] = useState<StatusType>();
   const [statusstr, setStatusStr] = useState<string>("offline");
 
- const [isMUserOpsOpen, setMUserOpsOpen] = useState(false);
+  const [isMUserOpsOpen, setMUserOpsOpen] = useState(false);
   const openMUserOps = useCallback(() => {
     setMUserOpsOpen(true);
   }, []);
@@ -44,7 +44,7 @@ const ProfileOtherPage = ({ userId }: any ) => {
     }
   }, [userId]);
 
-   useEffect(() => {
+  useEffect(() => {
     if (userId) {
       // statusを取得
       makeAPIRequest<StatusType>("get", `/status/${userId}`)
@@ -59,22 +59,22 @@ const ProfileOtherPage = ({ userId }: any ) => {
           console.error("Error:", error.message);
         });
     }
-   }, []);
+  }, []);
 
-    useEffect(() => {
-      if (userId) {
-        if (status && status.status !== "") {
-          setStatusStr(status.status);
-      }   
+  useEffect(() => {
+    if (userId) {
+      if (status && status.status !== "") {
+        setStatusStr(status.status);
+      }
     }
-   }, [status]);
-      
-    if (user?.avatar) {
-      setIcon(`http://localhost:3000/api/users/avatar/${userId}`);
-    }
-  
-    return (
-      <> 
+  }, [status]);
+
+  if (user?.avatar) {
+    setIcon(`http://localhost:3000/api/users/avatar/${userId}`);
+  }
+
+  return (
+    <>
       <div className="relative h-screen w-full overflow-y-auto bg-darkslategray-100 text-left font-body text-xl text-base-white">
         <HeaderMenu />
         <div className="absolute left-[500px] top-[220px] tracking-[0.1em]">
@@ -86,24 +86,22 @@ const ProfileOtherPage = ({ userId }: any ) => {
           src="/line-1.svg"
         />
         <div className="absolute left-[492px] top-[350px] flex h-[45px] w-[439px] flex-row items-center justify-start  gap-[25px] text-17xl">
-            <img
-              className="relative h-[45px] w-[45px] rounded-full cursor-pointer rounded-full"
-              alt=""
-              src={icon}
-              onClick={openMUserOps}
-            />
-            <div className="relative w-[175px] truncate tracking-[0.1em]">
-              {user?.username}
-            </div>
-            <div className="relative w-[70px] text-5xl">
-              {statusstr}
-            </div>
+          <img
+            className="relative h-[45px] w-[45px] cursor-pointer rounded-full rounded-full"
+            alt=""
+            src={icon}
+            onClick={openMUserOps}
+          />
+          <div className="relative w-[175px] truncate tracking-[0.1em]">
+            {user?.username}
           </div>
+          <div className="relative w-[70px] text-5xl">{statusstr}</div>
+        </div>
         <img
           className="absolute left-[470px] top-[473px] h-0.5 w-[500px]"
           alt=""
           src="/line-2.svg"
-          />
+        />
         <RankingContainer userId={userId} />
         <img
           className="absolute left-[470px] top-[768px] h-0.5 w-[500px]"
@@ -112,7 +110,7 @@ const ProfileOtherPage = ({ userId }: any ) => {
         />
         <MatchHistoryContainer userId={userId} />
       </div>
-        {isMUserOpsOpen && (
+      {isMUserOpsOpen && (
         <ModalPopup
           overlayColor="rgba(113, 113, 113, 0.3)"
           placement="Centered"
