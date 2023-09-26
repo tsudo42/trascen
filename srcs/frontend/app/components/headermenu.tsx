e460994e9b00c6be152ccb2f5341dce67e1c44cc
Anonymous <anonymous@example.com>
2023-09-26 20:17:21 +0900

    :bug: 画像取得のパスの間違いを修正

--- srcs/frontend/app/components/headermenu.tsx
+++ srcs/frontend/app/components/headermenu.tsx
@@ -16,7 +16,7 @@ const HeaderMenu: NextPage = () => {
 
   const [user, setUser] = useState<UserType>();
   const [icon, setIcon] = useState<string>(
-    "http://localhost:3000/api/users/avatar/0,
+    `http://localhost:3000/api/users/avatar/${profile.userId}`
   );
   const [timer, setTimer] = useState<number>(0);
 
