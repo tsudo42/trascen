955d43caf9424a4d31ea0e1850e04cbcc6ed71ff
Anonymous <anonymous@example.com>
2023-09-30 23:22:40 +0900

    :bug: 下に白い空白が出るエラーの修正

--- srcs/frontend/app/(dashboard)/profile/me/blocked/blocked-page.tsx
+++ srcs/frontend/app/(dashboard)/profile/me/blocked/blocked-page.tsx
@@ -27,32 +27,30 @@ const FriendsBlockedPage: NextPage = () => {
 
   return (
     <>
-      <div className="relative h-screen w-full bg-darkslategray-100 overflow-y-auto text-left font-body text-5xl text-base-white">
+      <div className="relative h-screen w-full overflow-y-auto bg-darkslategray-100 text-left font-body text-5xl text-base-white">
         <HeaderMenu />
-        <div className="absolute left-[470px] top-[327px] h-screen w-[500px] text-xl">
-          <div className="absolute left-[calc(50%_-_250px)] top-[-5px] h-[52px] w-[500px]">
-            <img
-              className="absolute left-[calc(50%_-_250px)] top-[49.5px] h-[5px] w-[500px]"
-              alt=""
-              src="/line-1.svg"
-            />
-            <button
-              className="absolute left-[calc(50%_-_183px)] top-[5px] inline-block cursor-pointer bg-[transparent] p-0 text-left font-body text-xl tracking-[0.1em] text-darkgray-300 [border:none]"
-              onClick={onAllClick}
-            >
-              All
-            </button>
-            <div className="absolute left-[calc(50%_-_68px)] top-[0px] h-8 w-[119px] bg-dimgray-100" />
-            <div className="absolute left-[calc(50%_-_48px)] top-[5px] tracking-[0.1em]">
-              Blocked
-            </div>
-            <button
-              className="absolute left-[calc(50%_+_101px)] top-[5px] inline-block cursor-pointer bg-[transparent] p-0 text-left font-body text-xl tracking-[0.1em] text-darkgray-300 [border:none]"
-              onClick={openMAddFriendPopup}
-            >
-              add friend
-            </button>
+        <div className="absolute left-[470px] top-[327px] h-[52px] w-[500px] text-xl">
+          <img
+            className="absolute left-[calc(50%_-_250px)] top-[49.5px] h-[5px] w-[500px]"
+            alt=""
+            src="/line-1.svg"
+          />
+          <button
+            className="absolute left-[calc(50%_-_183px)] top-[5px] inline-block cursor-pointer bg-[transparent] p-0 text-left font-body text-xl tracking-[0.1em] text-darkgray-300 [border:none]"
+            onClick={onAllClick}
+          >
+            All
+          </button>
+          <div className="absolute left-[calc(50%_-_68px)] top-[0px] h-8 w-[119px] bg-dimgray-100" />
+          <div className="absolute left-[calc(50%_-_48px)] top-[5px] tracking-[0.1em]">
+            Blocked
           </div>
+          <button
+            className="absolute left-[calc(50%_+_101px)] top-[5px] inline-block cursor-pointer bg-[transparent] p-0 text-left font-body text-xl tracking-[0.1em] text-darkgray-300 [border:none]"
+            onClick={openMAddFriendPopup}
+          >
+            add friend
+          </button>
         </div>
         <BlockedList />
       </div>
