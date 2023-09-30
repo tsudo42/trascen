f8930e20dfd4bc27cd5a1bcf7c3e48aec256c9eb
tsudo42 <91622558+tsudo42@users.noreply.github.com>
2023-10-01 06:01:53 +0900

    :heavy_minus_sign: remove test

--- srcs/frontend/app/(dashboard)/test/action/fetchProfile.ts
+++
@@ -1,9 +0,0 @@
-"use server";
-
-import makeAPIRequest from "@/app/api/api";
-
-export type ProfileType = { userId: string; bio: string };
-
-export async function fetchProfile() {
-  return await makeAPIRequest<ProfileType>("get", "/profiles");
-}
